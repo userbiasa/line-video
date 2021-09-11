f7e8c9fbcb5ac87fa1c25b0eda7d27b5cf564e60
userbiasa <90073608+userbiasa@users.noreply.github.com>
2021-09-11 19:41:24 +0700

    Update timeline.js

--- timeline.js
+++ timeline.js
@@ -2,7 +2,7 @@ async function onGetTimeline() {
     "use strict";
     document.getElementById('link_download').innerHTML = "<p>Processing...</p>";
     try {
-        const url = "https://api.allorigins.win/raw?url=" + encodeURIComponent(document.getElementById("urlTimeline").value);
+        const url = "https://apiorigins.herokuapp.com/?url=" + encodeURIComponent(document.getElementById("urlTimeline").value);
         const htmlString = (await (await fetch(url)).text());
         const html = new DOMParser().parseFromString(htmlString, "text/html");
         const json = JSON.parse(html.getElementById("__NEXT_DATA__").textContent);
