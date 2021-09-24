4eb5c418a5f15b751adb7b82288880ff7f8c4e76
userbiasa <90073608+userbiasa@users.noreply.github.com>
2021-09-25 01:04:17 +0700

    Update timeline.js

--- timeline.js
+++ timeline.js
@@ -2,21 +2,9 @@ async function onGetTimeline() {
     "use strict";
     document.getElementById('link_download').innerHTML = "<p>Processing...</p>";
     try {
-        const url = "https://apiorigins.herokuapp.com/?url=" + encodeURIComponent(document.getElementById("urlTimeline").value);
+        const url = "https://apiorigins.herokuapp.com/line?url=" + encodeURIComponent(document.getElementById("urlTimeline").value);
         const htmlString = (await (await fetch(url)).text());
-        const html = new DOMParser().parseFromString(htmlString, "text/html");
-        const json = JSON.parse(html.getElementById("__NEXT_DATA__")?.textContent || "{}");
-        const feeds = Object.values(json.props?.pageProps?.initialState?.api?.feeds || {});
-        const hasil = [];
-        feeds.forEach(({ post }) => {
-            let media = post?.contents?.media;
-            Array.isArray(media) && media.forEach(({ type, resourceId }) => {
-                type == "VIDEO" ?
-                    hasil.push(`<video width="320" height="240" controls><source src="https://obs.line-scdn.net/${resourceId}" type="video/mp4"></video>`) :
-                    hasil.push(`<img src="https://obs.line-scdn.net/${resourceId}" width="320" height="240">`);
-            });
-        });
-        document.getElementById('link_download').innerHTML = hasil.join("").trim() || '<p>Invalid Link!</p>';
+        document.getElementById('link_download').innerHTML = htmlString;
     } catch (error) {
         document.getElementById('link_download').innerHTML = `<p>Error : ${error.message}</p>`;
     }
